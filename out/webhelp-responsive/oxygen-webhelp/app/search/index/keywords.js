1fdc43cb994aca63dfac5fcbe808856b7a17a275
AnnaBureta <vrooblevskah@gmail.com>
2022-04-19 22:59:31 +0200

    first build

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,21 @@
+define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Glossary",p:["p1"]},{w:"Consumer",p:["p2"]},{w:"guide",p:["p2","p3","p4"]},{w:"Maintenance",p:["p3"]},{w:"ConsumerMaintenance",p:["p4"]},{w:"Coffees",p:["p5"]},{w:"to",p:["p5"]},{w:"try",p:["p5"]},{w:"Assembling",p:["p6"]},{w:"Cleaning",p:["p7","p8","p9","p10"]},{w:"the",p:["p7","p8","p9","p10","p11","p12","p13"]},{w:"KW100",p:["p7","p9","p12"]},{w:"filter",p:["p7","p8"]},{w:"holder",p:["p7","p8"]},{w:"and",p:["p7","p8","p9","p10"]},{w:"portafilter",p:["p7","p8"]},{w:"KW200",p:["p8","p10","p11"]},{w:"steam",p:["p9","p10"]},{w:"wand",p:["p9","p10"]},{w:"frothing",p:["p9","p10"]},{w:"attachment",p:["p9","p10"]},{w:"Operating",p:["p11","p12","p13"]},{w:"Kawomator",p:["p11","p12","p13"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4, 5];
+ph["p3"]=[6, 5];
+ph["p4"]=[7, 5];
+ph["p5"]=[8, 9, 10];
+ph["p6"]=[11];
+ph["p7"]=[12, 13, 14, 15, 16, 17, 18];
+ph["p8"]=[12, 13, 19, 15, 16, 17, 18];
+ph["p9"]=[12, 13, 14, 20, 21, 17, 22, 23];
+ph["p10"]=[12, 13, 19, 20, 21, 17, 22, 23];
+ph["p12"]=[24, 13, 25, 14];
+ph["p11"]=[24, 13, 25, 19];
+ph["p13"]=[24, 13, 25];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {3:-1,14:3,9:3,15:3,12:2,7:3,11:3,2:-1,13:2,6:3,8:3,10:3,5:2};
+});
